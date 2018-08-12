cc1b83a67bddf45b1d302172c1d09d478c0ab4f2
gualterapple <gualterapple@hotmail.com>
2018-08-12 14:21:28 -0300

    Tentando reconher os atributos Accept, Error e EmailDefinition vou mandar para o Mac

--- Lands/ViewModels/LoginViewModel.cs
+++ Lands/ViewModels/LoginViewModel.cs
@@ -78,7 +78,7 @@
             {
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
-                    "Insert an email",
+                    "You must enter an email",
                     "Accept");
                 return;
             }
